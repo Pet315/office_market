73c4f056d2af6b29a828175428a5cad46aa662e7
Pet315 <oppapeter17@gmail.com>
2023-03-29 22:48:11 +0300

    Add changes

--- main/views.py
+++ main/views.py
@@ -1,17 +1,16 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
+from main.models import Product
 
 
-# Create your views here.
 def main(request):
-    # return HttpResponse('<h1>Hello</h1>')
-    return render(request, 'main_page.html')
+    products = Product.objects.filter()
+    return render(request, 'main_page.html', context={'products': products})
 
 
 # def post_number(request, number: int):
 #     return HttpResponse(f'<h1>Post #{number}</h1>')
-#
-#
+
 # def post_number_month(request, number: int, month: int):
 #     if 1 <= month <= 12:
 #         return HttpResponse(f'<h1>Post #{number} from month #{month}</h1>')
