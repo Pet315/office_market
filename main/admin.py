3cf194772a68ec0d7809a47e4548cc92e24b2347
Pet315 <oppapeter17@gmail.com>
2023-03-30 02:18:24 +0300

    Add changes

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from main.models import Product
+from main.models import Product, Category
 
 admin.site.register(Product)
+admin.site.register(Category)
--- main/models.py
+++ main/models.py
@@ -1,11 +1,27 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=70)
+    # desc = models.TextField(max_length=470)
+    # photo = models.ImageField(upload_to='categories/')
+    # is_visible = models.BooleanField(default=True)
+
+    # def __iter__(self):
+    #     for product in self.products.all():
+    #         yield product
+
+    def __str__(self):
+        return f'{self.name}'
+
+
 class Product(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=70)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    desc = models.TextField(max_length=500, blank=True)
-    # photo = models.ImageField(upload_to='dishes/')
+    desc = models.TextField(max_length=470)
+    # photo = models.ImageField(upload_to='products/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    # is_visible = models.BooleanField(default=True)
 
     def __str__(self):
         return f'{self.name}'
