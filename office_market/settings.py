75ad6282596c6f157a208183a70fd4f7226173d9
Pet315 <oppapeter17@gmail.com>
2023-04-26 21:34:04 +0300

    Add changes

--- office_market/settings.py
+++ office_market/settings.py
@@ -33,7 +33,7 @@ DB_PORT = os.environ.get('DB_PORT')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['office-market.herokuapp.com']
 
 
 # Application definition
